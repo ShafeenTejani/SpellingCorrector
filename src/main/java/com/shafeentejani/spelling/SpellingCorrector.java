062c343a971c1af17d98845b1d58ac6b48eb535a
Shafeen Tejani <tejani19@gmail.com>
2011-12-21 21:17:33 +0000

    added logic to check for valid one-level edit distances and then valid two-level edit distances

--- src/main/java/com/shafeentejani/spelling/SpellingCorrector.java
+++ src/main/java/com/shafeentejani/spelling/SpellingCorrector.java
@@ -1,6 +1,12 @@
 package com.shafeentejani.spelling;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 
 public class SpellingCorrector {
 
@@ -18,9 +24,30 @@ public class SpellingCorrector {
 		return null;
 	}
 	
-	public List<String> suggestions(String word){
-		return null;
+	public Collection<String> suggestions(String word){
+		
+		if(isSpeltCorrectly(word)) return new ArrayList<String>();
+		
+		List<String> oneLevelEdits = dictionary.getOneLevelEdits(word);
+		
+		List<String> filtered = ImmutableList.copyOf(Collections2.filter(oneLevelEdits, wordInDictionary));
+		
+		if(!filtered.isEmpty()) return filtered;
+		
+		List<String> twoLevelEdits = new ArrayList<String>();
+		
+		for(String edit : oneLevelEdits){
+			twoLevelEdits.addAll(dictionary.getOneLevelEdits(edit));
+		}
+		
+		return ImmutableList.copyOf(Collections2.filter(twoLevelEdits, wordInDictionary));
+			
 	}
 
 	
+	private final Predicate<String> wordInDictionary = new Predicate<String>() {
+		public boolean apply(String word) {
+			return dictionary.containsWord(word);
+		}
+	};
 }
--- src/test/java/com/shafeentejani/spelling/SpellingCorrectorTest.java
+++ src/test/java/com/shafeentejani/spelling/SpellingCorrectorTest.java
@@ -2,6 +2,7 @@ package com.shafeentejani.spelling;
 
 import static junit.framework.Assert.*;
 import static org.mockito.Mockito.*;
+import static com.shafeentejani.test.Utils.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -34,5 +35,36 @@ public class SpellingCorrectorTest {
 		when(mockDictionary.containsWord("gorganzola")).thenReturn(false);
 		
 		assertFalse(corrector.isSpeltCorrectly("gorganzola"));
+	}
+	
+	@Test
+	public void testSuggestionsForCorrectlySpeltWord(){
+		
+		when(mockDictionary.containsWord("bear")).thenReturn(true);
+		
+		assertTrue(corrector.suggestions("bear").isEmpty());
+	}
+	
+	@Test
+	public void testSuggestionsOneEditDistance(){
+		
+		when(mockDictionary.containsWord("acd")).thenReturn(false);
+		when(mockDictionary.getOneLevelEdits("acd")).thenReturn(toList("cd","ad","ac","aca","ace","and"));
+		when(mockDictionary.containsWord("ace")).thenReturn(true);
+		when(mockDictionary.containsWord("and")).thenReturn(true);
+		
+		assertEquals(toList("ace","and"),corrector.suggestions("acd"));
+	}
+	
+	@Test
+	public void testSuggestionsTwoEditDistance(){
+		
+		when(mockDictionary.containsWord("yax")).thenReturn(false);
+		when(mockDictionary.getOneLevelEdits("yax")).thenReturn(toList("ya","ax","yx","cax","hax","ybx"));
+		when(mockDictionary.getOneLevelEdits("cax")).thenReturn(toList("can", "cab","cas","cay"));
+		when(mockDictionary.containsWord("can")).thenReturn(true);
+		when(mockDictionary.containsWord("cab")).thenReturn(true);
+		
+		assertEquals(toList("can","cab"),corrector.suggestions("yax"));
 	}
 }
