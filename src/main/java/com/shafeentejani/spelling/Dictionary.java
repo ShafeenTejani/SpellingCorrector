89a008e3487844e7df88eaaed85723f65563300d
Shafeen Tejani <tejani19@gmail.com>
2011-12-21 21:16:25 +0000

    determine words that are one edit distance away

--- src/main/java/com/shafeentejani/spelling/Dictionary.java
+++ src/main/java/com/shafeentejani/spelling/Dictionary.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,14 +13,17 @@ public class Dictionary {
 
 	private Set<String> knownWords = new HashSet<String>();
 	
+	private static final char[] ALPHABET = new char[]{'a','b','c','d','e','f','g','h','i','j',
+													  'k','l','m','n','o','p','q','r','s','t',
+													  'u','v','w','x','y','z'};
+	
 	public boolean containsWord(String word){ 
-		return knownWords.contains(word);
+		return knownWords.contains(word.toLowerCase());
 	}
 	
 	public void initialiseWords(List<String> words){
-		for(String word : words){
-			knownWords.add(word);
-		}
+		for(String word : words)
+			addWord(word);
 	}
 
 	public void initialiseWords(File wordsFile) throws IOException {
@@ -29,10 +33,71 @@ public class Dictionary {
 		String line;
 		
 		while((line = reader.readLine()) != null)
-			knownWords.add(line.trim().intern());
+			addWord(line.trim());
 		
 		reader.close();
 		
 	};
+
+	private void addWord(String word){
+		knownWords.add(word.toLowerCase().intern());
+	}
+	
+	public List<String> getOneLevelEdits(String word){
+		
+		List<String> edits = new ArrayList<String>();
+		
+		determineDeletes(word, edits);
+		
+		determineInserts(word, edits);
+		
+		determineReplacements(word, edits);
+		
+		determineTranspositions(word, edits);
+
+		return edits;
+	}
+
+	
+
+	private void determineTranspositions(String word, List<String> edits) {
+		char[] arr = word.toCharArray();
+
+		for(int i=0; i < word.length() -1; i++){
+			swap(arr, i, i+1);
+			
+			edits.add(String.valueOf(arr));
+			
+			swap(arr, i, i+1);
+		}
+	}
+	
+	private void determineReplacements(String word, List<String> edits) {
+		for(int i=0; i < word.length(); i++){
+			for(char c : ALPHABET){
+				edits.add(word.substring(0,i)+c+word.substring(i+1));
+			}
+		}
+	}
+
+	private void determineInserts(String word, List<String> edits) {
+		for(int i=0; i <= word.length(); i++){
+			for(char c : ALPHABET){
+				edits.add(word.substring(0,i)+c+word.substring(i));
+			}
+		}
+	}
+
+	private void determineDeletes(String word, List<String> edits) {
+		for(int i=0; i < word.length(); i++){
+			edits.add(word.substring(0, i) + word.substring(i+1));
+		}
+	}
+	
+	private void swap(char[] arr, int i, int j) {
+		char temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
 	
 }
--- src/test/java/com/shafeentejani/spelling/DictionaryTest.java
+++ src/test/java/com/shafeentejani/spelling/DictionaryTest.java
@@ -30,6 +30,12 @@ public class DictionaryTest {
 	}
 	
 	@Test
+	public void testDictionaryIsCaseInsensitive(){
+		assertTrue(dictionary.containsWord("Gorgonzola"));
+		assertTrue(dictionary.containsWord("gorgOnzolA"));
+	}
+	
+	@Test
 	public void testNonExistingWord(){
 		assertFalse(dictionary.containsWord("gorganzola"));
 	}
@@ -62,5 +68,53 @@ public class DictionaryTest {
 			assertTrue("Dictionary does not contain word: "+line.trim(),
 					dictionary.containsWord(line.trim()));
 		
+	}
+	
+	
+	@Test
+	public void testOneWordEdits(){
+		assertEquals(getExpectedEdits(), dictionary.getOneLevelEdits("cats"));
+	}
+	
+	
+	private List<String> getExpectedEdits(){
+		return toList("ats","cts","cas","cat",
+				"acats","bcats","ccats","dcats","ecats","fcats","gcats",
+				"hcats","icats","jcats","kcats","lcats","mcats","ncats",
+				"ocats","pcats","qcats","rcats","scats","tcats","ucats",
+				"vcats","wcats","xcats","ycats","zcats",
+				"caats","cbats","ccats","cdats","ceats","cfats","cgats",
+				"chats","ciats","cjats","ckats","clats","cmats","cnats",
+				"coats","cpats","cqats","crats","csats","ctats","cuats",
+				"cvats","cwats","cxats","cyats","czats",
+				"caats","cabts","cacts","cadts","caets","cafts","cagts",
+				"cahts","caits","cajts","cakts","calts","camts","cants",
+				"caots","capts","caqts","carts","casts","catts","cauts",
+				"cavts","cawts","caxts","cayts","cazts",
+				"catas","catbs","catcs","catds","cates","catfs","catgs",
+				"caths","catis","catjs","catks","catls","catms","catns",
+				"catos","catps","catqs","catrs","catss","catts","catus",
+				"catvs","catws","catxs","catys","catzs",
+				"catsa","catsb","catsc","catsd","catse","catsf","catsg",
+				"catsh","catsi","catsj","catsk","catsl","catsm","catsn",
+				"catso","catsp","catsq","catsr","catss","catst","catsu",
+				"catsv","catsw","catsx","catsy","catsz",
+				"aats","bats","cats","dats","eats","fats","gats",
+				"hats","iats","jats","kats","lats","mats","nats",
+				"oats","pats","qats","rats","sats","tats","uats",
+				"vats","wats","xats","yats","zats",
+				"cats","cbts","ccts","cdts","cets","cfts","cgts",
+				"chts","cits","cjts","ckts","clts","cmts","cnts",
+				"cots","cpts","cqts","crts","csts","ctts","cuts",
+				"cvts","cwts","cxts","cyts","czts",
+				"caas","cabs","cacs","cads","caes","cafs","cags",
+				"cahs","cais","cajs","caks","cals","cams","cans",
+				"caos","caps","caqs","cars","cass","cats","caus",
+				"cavs","caws","caxs","cays","cazs",
+				"cata","catb","catc","catd","cate","catf","catg",
+				"cath","cati","catj","catk","catl","catm","catn",
+				"cato","catp","catq","catr","cats","catt","catu",
+				"catv","catw","catx","caty","catz",
+				"acts","ctas","cast");
 	}
 }
